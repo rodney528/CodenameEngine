95b4d888f0f184f34fd94b5d3d1a4a2333d3dedc
Lasy <131571752+lasystuff@users.noreply.github.com>
2025-10-05 11:51:50 +0900

    Push unique event to Event List Once (#785)
    * Update EventsData.hx
    
    * Update EventsData.hx again

--- source/funkin/backend/chart/EventsData.hx
+++ source/funkin/backend/chart/EventsData.hx
@@ -164,25 +164,28 @@ class EventsData {
 
 			if (fileTxt.trim() == "") continue;
 
-			eventsList.push(eventName);
-			eventsParams.set(eventName, []);
+			if (!eventsList.contains(eventName))
+			{
+				eventsList.push(eventName);
+				eventsParams.set(eventName, []);
 
-			try {
-				var data:EventInfoFile = cast Json.parse(fileTxt);
-				if (data == null || data.params == null) continue;
+				try {
+					var data:EventInfoFile = cast Json.parse(fileTxt);
+					if (data == null || data.params == null) continue;
 
-				var finalParams:Array<EventParamInfo> = [];
-				for (paramData in data.params) {
-					try {
-						finalParams.push({
-							name: paramData.name,
-							type: hscriptInterp.expr(hscriptParser.parseString(paramData.type)),
-							defValue: paramData.defaultValue
-						});
-					} catch (e) {trace('Error parsing event param ${paramData.name} - ${eventName}: $e'); finalParams.push(null);}
-				}
-				eventsParams.set(eventName, finalParams);
-			} catch (e) {trace('Error parsing file $file: $e');}
+					var finalParams:Array<EventParamInfo> = [];
+					for (paramData in data.params) {
+						try {
+							finalParams.push({
+								name: paramData.name,
+								type: hscriptInterp.expr(hscriptParser.parseString(paramData.type)),
+								defValue: paramData.defaultValue
+							});
+						} catch (e) {trace('Error parsing event param ${paramData.name} - ${eventName}: $e'); finalParams.push(null);}
+					}
+					eventsParams.set(eventName, finalParams);
+				} catch (e) {trace('Error parsing file $file: $e');}
+			}
 		}
 
 		hscriptInterp = null; hscriptParser = null;
