8f09809e6988364e2fac149881c6fcb5744490d4
Ralty <78720179+Raltyro@users.noreply.github.com>
2025-11-16 16:23:17 +0700

    Re-apply this code cuz im a dumbass

--- source/funkin/game/PlayState.hx
+++ source/funkin/game/PlayState.hx
@@ -1587,11 +1587,13 @@ class PlayState extends MusicBeatState
 				var camera:FlxCamera = event.params[1] == "camHUD" ? camHUD : camGame;
 				camera.zoom += event.params[0];
 			case "Camera Bop":
-				if (useCamZoomMult) {
-					camZoomingMult += event.params[0];
-				} else {
-					FlxG.camera.zoom += event.params[0] * camZoomingStrength;
-					camHUD.zoom += event.params[0] * camZoomingStrength;
+				if (Options.camZoomOnBeat) {
+					if (useCamZoomMult) {
+						camZoomingMult += event.params[0];
+					} else {
+						FlxG.camera.zoom += event.params[0] * camZoomingStrength;
+						camHUD.zoom += event.params[0] * camZoomingStrength;
+					}
 				}
 			case "Camera Zoom":
 				var cam = event.params[2] == "camHUD" ? camHUD : camGame;
