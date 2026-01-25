f8f9d6eb4abf76eacbfd981fd558ee22e3befda6
HEIHUAa <112499486+HEIHUAa@users.noreply.github.com>
2026-01-25 19:18:18 +0800

    Use fastSinCos for notes (#889)
    * Update Strum.hx
    
    * Update Strum.hx

--- source/funkin/game/Strum.hx
+++ source/funkin/game/Strum.hx
@@ -174,8 +174,9 @@ class Strum extends FlxSprite {
 				if (daNote.isSustainNote) offset.y -= height * 0.5;
 
 				if (Std.int(daNote.__noteAngle % 360) != 0) {
-					var noteAngleCos = FlxMath.fastCos(daNote.__noteAngle / PIX180);
-					var noteAngleSin = FlxMath.fastSin(daNote.__noteAngle / PIX180);
+					var noteAngle = FlxMath.fastSinCos(daNote.__noteAngle / PIX180);
+					var noteAngleCos = noteAngle.cos;
+					var noteAngleSin = noteAngle.sin;
 
 					var aOffset:FlxPoint = FlxPoint.get(
 						(daNote.origin.x / daNote.scale.x) - daNote.offset.x,
